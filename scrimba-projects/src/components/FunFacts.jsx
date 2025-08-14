abaa1d500b4804d1b731d10ae7bca409cad2c874
Mahdi Khrrousheh <mahdi.khrrousheh@gmail.com>
2025-08-14 22:43:05 +0300

    clean up funfact card

--- scrimba-projects/src/components/FunFacts.jsx
+++ scrimba-projects/src/components/FunFacts.jsx
@@ -4,14 +4,16 @@ import "./FunFacts.css"
 function FunfactsReact() {
     return (
         <div className='card'>
-            <img src={reactLogo} alt="react logo" width="40px" />
-            <img src={reactLogo} alt="bacground card react logo" className='shadow-image'/>
-            <h3>Those are fun facts about ReactJS:</h3>
+            <nav className='card-nav'>
+                <img src={reactLogo} alt="react logo" width="40px" />
+                <h3>Fun facts about ReactJS:</h3>
+            </nav>
             <ul>
                 <li><strong>Build UIs with pieces</strong> – You make small, reusable “components” (like Lego blocks) </li>
                 <li><strong>Fast updates</strong> – React updates only what changes, so it feels quick.</li>
                 <li><strong>Works everywhere</strong> – You can use React as native code.</li>
             </ul>
+            <img src={reactLogo} alt="bacground card react logo" className='shadow-image'/>
         </div>
     )
 }
