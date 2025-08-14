d3b439ce7ba4eb2456c9e3c3d677855ead01bd40
Mahdi Khrrousheh <mahdi.khrrousheh@gmail.com>
2025-08-14 18:25:20 +0300

    static page section: fun facts about react

---
+++ scrimba-projects/src/components/fun_facts.jsx
@@ -0,0 +1,19 @@
+import reactLogo from '../assets/react.svg'
+import "./fun_facts.css"
+
+function FunfactsReact() {
+    return (
+        <div className='card'>
+            <img src={reactLogo} alt="react logo" width="40px" />
+            <img src={reactLogo} alt="bacground card react logo" className='shadow-image'/>
+            <h3>Those are fun facts about ReactJS:</h3>
+            <ul>
+                <li><strong>Build UIs with pieces</strong> – You make small, reusable “components” (like Lego blocks) </li>
+                <li><strong>Fast updates</strong> – React updates only what changes, so it feels quick.</li>
+                <li><strong>Works everywhere</strong> – You can use React as native code.</li>
+            </ul>
+        </div>
+    )
+}
+
+export default FunfactsReact;
